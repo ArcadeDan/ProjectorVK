bf6689bf1a396f5eb9c05ed3f93468668f451002
Dan Sparks <lordaniel24@gmail.com>
2023-02-18 16:24:15 -0600

    its borked

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
-use std::time::Duration;
 
-use sdl2::{pixels::Color, keyboard::Keycode, event::Event};
+extern crate sdl2;
+extern crate vulkano;
+
+use std::{ffi::CString, time::Duration};
+
+use sdl2::{event::Event, keyboard::Keycode, pixels::Color, video::VkInstance};
+use vulkano::instance::{Instance, InstanceExtensions};
+use vulkano::swapchain::Surface;
+use vulkano::{Handle, Version, VulkanObject};
 
 fn main() -> Result<(), String> {
     //println!("Goodbye cruel world...");
@@ -8,12 +15,24 @@ fn main() -> Result<(), String> {
     let sdl_ctx = sdl2::init()?;
     let video_subsys = sdl_ctx.video()?;
     //::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 30));
+
     let window = video_subsys
         .window("Test", 800, 600)
+        .vulkan()
         .position_centered()
         .build()
         .map_err(|e| e.to_string())?;
-        
+
+    let instance_extensions_strings: Vec<CString> = window
+        .vulkan_instance_extensions()
+        .unwrap()
+        .iter()
+        .map(|&v| CString::new(v).unwrap())
+        .collect();
+
+    
+    let instance_extension = InstanceExtensions::from(instance_extensions_strings.iter().map(AsRef::as_ref))
+    
     let mut canvas = window
         .into_canvas()
         .build()
@@ -35,14 +54,11 @@ fn main() -> Result<(), String> {
                 } => break 'running,
                 _ => {}
             }
-            
         }
         canvas.clear();
         canvas.present();
-        ::std::thread::sleep(Duration::new(1, 1_000_000_000u32 / 30));
+        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 30)); // 30 fps
     }
-
-    
 
     Ok(())
 }
