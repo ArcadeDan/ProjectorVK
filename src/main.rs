82a31162db0e7bf59fde1c10b35574e4b194940f
Dan Sparks <lordaniel24@gmail.com>
2023-02-18 15:43:29 -0600

    sdl2 context and window set up

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
-fn main() {
-    println!("Goodbye cruel world...");
+use sdl2::{pixels::Color, keyboard::Keycode, event::Event};
+
+fn main() -> Result<(), String> {
+    //println!("Goodbye cruel world...");
+    let sdl_ctx = sdl2::init()?;
+    let video_subsys = sdl_ctx.video()?;
+    let window = video_subsys
+        .window("Test", 800, 600)
+        .position_centered()
+        .build()
+        .map_err(|e| e.to_string())?;
+        
+    let mut canvas = window
+        .into_canvas()
+        .build()
+        .map_err(|e| e.to_string())?;
+
+    canvas.set_draw_color(Color::RGB(255, 0, 0));
+    canvas.clear();
+    canvas.present();
+
+    let mut event_pump = sdl_ctx.event_pump()?;
+
+    'running: loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => break 'running,
+                _ => {}
+            }
+        }
+    }
+
+    Ok(())
 }
